28e7d81e6501fb903066265804a15ad1bc96c375
Ye He <yehe@expediagroup.com>
2022-07-05 20:13:52 +1000

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Ye He on 5/7/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/TestFastlaneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestFastlaneApp.swift
+//  Shared
+//
+//  Created by Ye He on 5/7/2022.
+//
+
+import SwiftUI
+
+@main
+struct TestFastlaneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
